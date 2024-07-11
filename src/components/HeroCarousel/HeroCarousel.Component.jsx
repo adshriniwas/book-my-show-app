c76cb6ca5ba3e9d60216c37866013cc16f0a89ff
adshriniwas <49123961+adshriniwas@users.noreply.github.com>
2024-07-11 11:28:12 +0000

    Day2-part-2-commit

--- src/components/HeroCarousel/HeroCarousel.Component.jsx
+++ src/components/HeroCarousel/HeroCarousel.Component.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import Slider from "react-slick";
+
+const initImages = [
+  {
+    image:
+      "https://assets-in.bmscdn.com/promotions/cms/creatives/1720507931638_sundeepsharmaweb.jpg",
+  },
+  {
+    image:
+      "https://assets-in.bmscdn.com/promotions/cms/creatives/1720007270139_monsoonwebbanner.jpg",
+  },
+  {
+    image:
+      "https://assets-in.bmscdn.com/promotions/cms/creatives/1720522164226_srweb.jpg",
+  },
+  {
+    image:
+      "https://assets-in.bmscdn.com/promotions/cms/creatives/1717080055549_playcardweb.jpg",
+  },
+];
 
 const HeroCarousel = () => {
-  return <div>HeroCarousel.Component</div>;
+  const [images, setImages] = useState(initImages);
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
+  return (
+    <>
+      <div className="lg:hidden slider-container">
+        <Slider {...settings}>
+          {images.map((image) => {
+            return (
+              <div className="w-full h-56 md:h-80 py-3">
+                <img
+                  src={image.image}
+                  alt=""
+                  className="w-full h-full rounded-md object-cover"
+                />
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+      <div className="hidden lg:block"></div>
+    </>
+  );
 };
 
 export default HeroCarousel;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
 
 const HomePage = () => {
   const [recommendedMovies, setRecommendedMovies] = useState([]);
-  const [premierMovies, setPremierMovies] = useState([]);
+  const [premiereMovies, setPremiereMovies] = useState([]);
   const [onlineStreamEvents, setOnlineStreamEvents] = useState([]);
 
   return (
@@ -17,6 +17,39 @@ const HomePage = () => {
         <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3">
           The best of Entertainment
         </h1>
+        <EntertainmentCard />
+      </div>
+
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <PosterSlider
+          title="Recommended Movies"
+          subject="List of recommended movies"
+          posters={recommendedMovies}
+          isDark={false}
+        />
+      </div>
+
+      <div className="bg-premier-800 py-12">
+        <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+          <div className="hidden md:flex">
+            <img src="" alt="rupay" className="w-full h-full" />
+          </div>
+          <PosterSlider
+            title="Premieres"
+            subject="Brand new releases every friday"
+            posters={premiereMovies}
+            isDark={true}
+          />
+        </div>
+      </div>
+
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <PosterSlider
+          title="Online streaming events"
+          subject="online stream events"
+          posters={onlineStreamEvents}
+          isDark={false}
+        />
       </div>
     </>
   );
