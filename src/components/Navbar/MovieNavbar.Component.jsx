5e6af25b07896a6d22534f410c3dd34777a05556
adshriniwas <49123961+adshriniwas@users.noreply.github.com>
2025-01-18 13:25:09 +0000

    Day-05 Movie page half done

--- src/components/Navbar/MovieNavbar.Component.jsx
+++ src/components/Navbar/MovieNavbar.Component.jsx
@@ -1,7 +1,105 @@
-import React from "react";
+import React, { useContext } from "react";
+import { BiChevronDown, BiMenu, BiSearch } from "react-icons/bi";
+import { MovieContext } from "../../context/Movie.context";
+
+function NavSm() {
+
+  const {movie} = useContext(MovieContext);
+
+  return (
+    <>
+      <div className="text-white flex items-center justify-between">
+        <div>
+          <h3 className="text-xl font-bold">{movie.original_title ?? ""}</h3>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function NavMd() {
+  const {movie} = useContext(MovieContext);
+  return (
+    <>
+      {/* <div className="w-10 h-10">
+        <img src="" alt="logo" className="w-full h-full" />
+      </div> */}
+      <div className="w-full">
+          <h3 className="text-white text-xl font-bold">{movie.original_title ?? ""}</h3>
+        </div>
+      <div className="w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md">
+        <BiSearch />
+        <input
+          type="search"
+          className="w-full bg-transparent border-none focus:outline-none"
+        />
+      </div>
+      <div className="flex items-center justify-evenly">
+        <div className="ms-3 w-8 h-8 text-white">
+          <BiMenu className="w-full h-full" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function NavLg() {
+  const {movie} = useContext(MovieContext);
+  return (
+    <>
+      <div className="container flex mx-auto px-4 items-center justify-between">
+        <div className="flex items-center w-3/4 gap-3">
+          {/* <div className="w-10 h-10">
+            <img src="" alt="logo" className="w-full h-full" />
+          </div> */}
+          <div className="w-full">
+            <h3 className=" text-white text-xl font-bold">{movie.original_title ?? ""}</h3>
+          </div>
+          <div className="w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md">
+            <BiSearch />
+            <input
+              type="search"
+              className="w-full bg-transparent border-none focus: outline-none"
+              placeholder="Search for movies, events, plays, sports and activites"
+            />
+          </div>
+        </div>
+        <div className="flex items-center justify-evenly">
+          <select className="mx-3 bg-transparent text-white">
+            <option>Hospet</option>
+          </select>
+          <button className="bg-red-600 text-sm px-2 py-1 text-white rounded">Sign in</button>
+          <div className="ms-3 w-8 h-8 text-white">
+            <BiMenu className="w-full h-full" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 const MovieNavbar = () => {
-  return <div>MovieNavbar</div>;
+
+  return  (
+    <nav className="bg-darkBackground-700 px-4 py-3">
+      {/* Small Screen NavBar */}
+      {/* >= md its hidden, only visible on small screen size */}
+      <div className="md:hidden">
+        {/* <div className="sm:flex md:hidden lg:hidden"> */}
+        <NavSm />
+      </div>
+
+      {/* Medium Screen Size */}
+      <div className="hidden md:flex lg:hidden">
+        <NavMd />
+      </div>
+
+      {/* Large Screen Size */}
+      <div className="hidden md:hidden lg:flex">
+        <NavLg />
+      </div>
+    </nav>
+  );
 };
 
 export default MovieNavbar;
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,8 +1,175 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MovieLayoutHoc from "../layout/MovieLayoutHoc";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { MovieContext } from "../context/Movie.context";
+import { FaCcVisa, FaCcApplePay } from "react-icons/fa";
+import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
+import { NextArrow, PrevArrow } from "../components/HeroCarousel/Arrows.Component";
+
 
 const MoviePage = () => {
-  return <div>MoviePage</div>;
+
+  const [cast, setCast] = useState();
+  const [similarMovies, setSimilarMovies] = useState([]);
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
+  const {movie, setMovie} = useContext(MovieContext);
+
+  const {id} = useParams();
+  const movieCastOptions = {
+    method: 'GET',
+    url: `movie/${id}/credits`,
+    headers: {
+      accept: 'application/json',
+    }
+  };
+
+  const similarMoviesOptions = {
+    method: 'GET',
+    url: `movie/${id}/similar`,
+    headers: {
+      accept: 'application/json',
+    }
+  };
+
+  const recommendedMoviesOptions = {
+    method: 'GET',
+    url: `movie/${id}/recommendations`,
+    headers: {
+      accept: 'application/json',
+    }
+  };
+
+  const movieOptions = {
+    method: 'GET',
+    url: `movie/${id}`,
+    headers: {
+      accept: 'application/json',
+    }
+  };
+
+  useEffect(()=>{
+    const requestCast = async ()=>{
+      const getCast = await axios.request(movieCastOptions);
+      setCast(getCast.data.cast)
+    }
+
+    requestCast().catch((error) => {
+      console.error('Error:', error);
+    });
+  },[id]);
+
+  useEffect(()=>{
+    const requestSimilarMovies = async ()=>{
+      const getSimilarMovies = await axios.request(similarMoviesOptions);
+      setSimilarMovies(getSimilarMovies.data.results);
+    }
+    requestSimilarMovies().catch((error) => {
+      console.error('Error:', error);
+    });
+    
+  },[id]);
+
+  useEffect(()=>{
+    const requestRecommededMovies = async ()=>{
+      const getRecommendedMovies = await axios.request(recommendedMoviesOptions);
+      setRecommendedMovies(getRecommendedMovies.data.results);
+    }
+    requestRecommededMovies().catch((error) => {
+      console.error('Error:', error);
+    });
+    
+  },[id]);
+
+  useEffect(()=>{
+    const requestMovie = async ()=>{
+      const getMovie = await axios.request(movieOptions);
+      setMovie(getMovie.data);
+    }
+    requestMovie().catch((error) => {
+      console.error('Error:', error);
+    });
+    
+  },[id]);
+
+  
+
+  return (
+    <>
+    {/* <MovieHero/> */}
+      <div className="my-12 container px-4 lg-ml-20 lg:w-2/1">
+        <div className="flex flex-col items-start gap-3">
+          <h1 className="text-gray-800 font-bold gap-3 text-2xl">
+            About the movie
+          </h1>
+          <p>{movie.overview}</p>
+        </div>
+
+        <div className="my-8">
+          <hr />
+        </div>
+
+        <div className="my-8">
+          <h2 className="text-gray-800 font-bold text-2xl mb-3">
+            Applicable Offers
+          </h2>
+          <div className="flex flex-col gap-3 lg:flex-row ">
+            <div className="flex items-start gap-2 bg-yellow-100 p-3 border-yellow-400 border-dashed border-2 rounded-md">
+              <div className="w-8 h-8">
+                <FaCcVisa className="w-full h-full" />
+              </div>
+              <div className="flex flex-col items-start">
+                <h3 className="text-gray-700 text-xl font-bold">
+                  Visa Stream Offer
+                </h3>
+                <p className="text-gray-600">
+                  Get 75% off up to INR 200 on all RuPay Card* on BookMyShow
+                  Stream
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-2 bg-yellow-100 p-3 border-yellow-400 border-dashed border-2 rounded-md">
+              <div className="w-8 h-8">
+                <FaCcApplePay className="w-full h-full" />
+              </div>
+              <div className="flex flex-col items-start">
+                <h3 className="text-gray-700 text-xl font-bold">Film Pass</h3>
+                <p className="text-gray-600">
+                  Get 75% off up to INR 200 on all RuPay Card* on BookMyShow
+                  Stream
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="my-8">
+          <hr />
+        </div>
+
+        {/* Cast Sliders */}
+        <div className="my-8">
+          <PosterSlider
+            title="Similar Movies"
+            posters={similarMovies}
+            isDark={false}
+          />
+        </div>
+
+        {/* recommended movies Slider */}
+        <div className="my-8">
+          <hr />
+        </div>
+
+        <PosterSlider
+          title="Recommended Movies"
+          posters={recommendedMovies}
+          isDark={false}
+        />
+      </div>
+    </>
+  );
 };
 
 export default MovieLayoutHoc(MoviePage);
